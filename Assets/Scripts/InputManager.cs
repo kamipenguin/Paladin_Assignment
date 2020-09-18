340fa8f4cb07806ce9b83989585f8f1287908325
Roxanne van Dam <roxanne.vandam@outlook.com>
2020-09-18 02:29:12 +0200

    added animations and fixed IsGrounded

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -2,25 +2,16 @@
 
 public class InputManager : MonoBehaviour
 {
-    private Rigidbody2D _rigidBody;
-    [SerializeField]
-    private float _moveSpeed = 15f;
-    [SerializeField]
-    private float _jumpSpeed = 10f;
-    private bool _hasJumped;
-    private bool _isGrounded;
-    private float _rayXOffset = 0.9f;
+    private MovementController _movementController;
 
     private void Awake()
     {
-        _rigidBody = GetComponent<Rigidbody2D>();
+        _movementController = FindObjectOfType<MovementController>();
     }
 
     private void Update()
     {
-        //Debug.DrawRay(new Vector2(transform.position.x - _rayXOffset, transform.position.y), Vector2.down, Color.red);
         HandleInput();
-        IsGrounded();
     }
 
     /// <summary>
@@ -29,41 +20,12 @@ public class InputManager : MonoBehaviour
     private void HandleInput()
     {
         float horizontal = Input.GetAxis("Horizontal");
-        Walk(horizontal);
+        _movementController.Walk(horizontal);
+
+        //if (Input.GetAxis("Jump") > 0)
+        //    _movementController.Jump();
 
         if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W))
-            Jump();
-    }
-
-    private void Walk(float horizontal)
-    {
-        Vector2 direction = new Vector2(horizontal, 0);
-        _rigidBody.AddForce(direction * _moveSpeed);
-    }
-
-    private void Jump()
-    {
-        if (_isGrounded && !_hasJumped)
-        {
-            _hasJumped = true;
-            _rigidBody.AddForce(Vector2.up * _jumpSpeed, ForceMode2D.Impulse);
-        }
-    }
-
-    private void IsGrounded()
-    {
-        RaycastHit2D hit = Physics2D.Raycast(new Vector2(transform.position.x - _rayXOffset, transform.position.y), Vector2.down);
-        if (hit.collider != null)
-        {
-            if (hit.collider.gameObject.CompareTag("Ground"))
-            {
-                _hasJumped = false;
-                _isGrounded = true;
-            }
-            else
-                _isGrounded = false;
-        }
-        else
-            _isGrounded = false;
+            _movementController.Jump();
     }
 }
---
+++ Assets/Scripts/MovementController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+    private Rigidbody2D _rigidBody;
+    [SerializeField]
+    private float _moveSpeed = 15f;
+    [SerializeField]
+    private float _jumpSpeed = 10f;
+    private bool _hasJumped;
+    private bool _isGrounded;
+    private float _rayXOffset = 0.9f;
+
+    private void Awake()
+    {
+        _rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        Debug.DrawRay(new Vector2(transform.position.x - _rayXOffset, transform.position.y), Vector2.down, Color.red);
+    }
+
+    public void Walk(float horizontal)
+    {
+        Vector2 direction = new Vector2(horizontal, 0);
+        _rigidBody.AddForce(direction * _moveSpeed);
+    }
+
+    public void Jump()
+    {
+        if (IsGrounded() && !_hasJumped)
+        {
+            _hasJumped = true;
+            _rigidBody.AddForce(Vector2.up * _jumpSpeed, ForceMode2D.Impulse);
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(new Vector2(transform.position.x - _rayXOffset, transform.position.y), Vector2.down, 1);
+        if (hit.collider != null)
+        {
+            if (hit.collider.gameObject.CompareTag("Ground"))
+            {
+                _hasJumped = false;
+                return true;
+            }
+            else
+                return false;
+        }
+        else
+            return false;
+    }
+}
